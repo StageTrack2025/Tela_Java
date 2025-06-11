9d99bf8f1efdff86ffd5f025302658c4b818715f
StageTrack2025 <lucasmonteirowalger@gmail.com>
2025-06-11 20:54:43 -0300

    Add files via upload

---
+++ TelaInicio/src/main/java/com/mycompany/telainicio/TelaInicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telainicio;
+
+/**
+ *
+ * @author Lucas W
+ */
+public class TelaInicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
