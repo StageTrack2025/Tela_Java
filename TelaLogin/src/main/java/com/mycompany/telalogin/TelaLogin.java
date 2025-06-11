85aeae461d632ef64c2260cb6d49853b2460d1ef
StageTrack2025 <lucasmonteirowalger@gmail.com>
2025-06-11 19:10:34 -0300

    Add files via upload

---
+++ TelaLogin/src/main/java/com/mycompany/telalogin/TelaLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telalogin;
+
+/**
+ *
+ * @author Vitor
+ */
+public class TelaLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
