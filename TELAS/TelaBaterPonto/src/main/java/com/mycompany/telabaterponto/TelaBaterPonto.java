1e5f6b2a020a5dd299baad1ea4a991a9521b323d
StageTrack2025 <lucasmonteirowalger@gmail.com>
2025-06-16 20:21:43 -0300

    Add files via upload

---
+++ TELAS/TelaBaterPonto/src/main/java/com/mycompany/telabaterponto/TelaBaterPonto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telabaterponto;
+
+/**
+ *
+ * @author Lucas W
+ */
+public class TelaBaterPonto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TELAS/situacaoEs/src/main/java/com/mycompany/situacaoes/SituacaoEs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.situacaoes;
+
+/**
+ *
+ * @author aluno
+ */
+public class SituacaoEs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
