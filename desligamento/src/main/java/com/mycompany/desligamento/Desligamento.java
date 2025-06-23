7719e338bfda9f97c88cd787d2d5e491243e91ff
StageTrack2025 <lucasmonteirowalger@gmail.com>
2025-06-23 20:00:11 -0300

    Add files via upload

---
+++ desligamento/src/main/java/com/mycompany/desligamento/Desligamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.desligamento;
+
+/**
+ *
+ * @author aluno
+ */
+public class Desligamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
